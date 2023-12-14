4b523d3effd5c679873a6a41e1bf5a7fceaafbe1
Brian Maleki <hyporos@gmail.com>
2023-12-14 16:28:42 -0500

    Revert "Revert "Add Exponential Graph Option""
    This reverts commit 5caca022809b722b93905babe5d6bc56974b1ef0.

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -18,7 +18,9 @@ import { isValid, getRemainingSymbols, getDailySymbols } from "../../lib/utils";
 
 import dayjs from "dayjs";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
 
 import "./Graph.css";
 
@@ -54,7 +56,7 @@ type GraphSymbols = {
   name: string;
   level: number;
   entryLevel: number;
-  date: string;
+  date: string | number;
   power: number;
 };
 
@@ -71,9 +73,12 @@ const Graph = ({ symbols, swapped }: Props) => {
   const [flatDateSymbols, setFlatDateSymbols] = useState<FlatDateSymbols[]>([]);
   const [currentPower, setCurrentPower] = useState(NaN);
   const [maxPower, setMaxPower] = useState(NaN);
+  const [maxDays, setMaxDays] = useState(NaN);
   const [targetPower, setTargetPower] = useState(NaN);
   const [dateToPower, setDateToPower] = useState("");
   const [yAxisTicks, setYAxisTicks] = useState<number[]>([]);
+  const [xAxisTicks, setXAxisTicks] = useState<number[]>([]);
+  const [graphType, setGraphType] = useState("linear");
 
   const enabledSymbols = symbols.filter(
     (symbol) =>
@@ -184,23 +189,33 @@ const Graph = ({ symbols, swapped }: Props) => {
       console.error(e);
       setDateSymbols([]);
     }
-  }, [symbols, swapped]);
+  }, [symbols, swapped, graphType]);
 
   // Get and store the individual entries for the graph
-  useEffect(() => {
+  useLayoutEffect(() => {
     let tempPower = currentPower;
     const maxPowerByDate: Record<string, number> = {};
 
     // Create a flat (merged) version of the dateSymbols array.
     const tempFlatDateSymbols = dateSymbols
       .flatMap((symbol) =>
-        symbol.progress.map((entry) => ({
-          name: symbol.name,
           level: symbol.level,
           entryLevel: entry.level,
-          date: entry.date,
-          power: NaN,
-        }))
+        symbol.progress.map((entry) => {
+          // Find the difference of days between today and the entry date (days).
+          // If they both land on today, make sure it's set to 0 and not 1
+          const diffDays = dayjs().isSameOrAfter(entry.date, "day")
+            ? 0
+            : dayjs(entry.date).diff(dayjs(), "day") + 1;
+
+          return {
+            name: symbol.name,
+            level: symbol.level,
+            entryLevel: entry.level,
+            date: graphType === "exponential" ? diffDays : entry.date,
+            power: NaN,
+          };
+        })
       )
       .sort((a, b) => dayjs(a.date).diff(dayjs(b.date)))
       // If there is more than one entry on the same date, merge them and combine the power gain (+10)
@@ -233,13 +248,14 @@ const Graph = ({ symbols, swapped }: Props) => {
       name: "",
       level: NaN,
       entryLevel: NaN,
-      date: dayjs().format("YYYY-MM-DD"),
+      date: graphType === "exponential" ? 0 : dayjs().format("YYYY-MM-DD"),
       power: currentPower,
     });
 
     setFlatDateSymbols(tempFlatDateSymbols);
     setGraphSymbols(tempGraphSymbols);
     setMaxPower(tempGraphSymbols[tempGraphSymbols.length - 1]?.power);
+    setMaxDays(tempGraphSymbols[tempGraphSymbols.length - 1]?.date as number);
   }, [dateSymbols]);
 
   // Render the custom tooltip for the graph
@@ -257,9 +273,23 @@ const Graph = ({ symbols, swapped }: Props) => {
     const isFirstEntry = payload[0].value === currentPower;
 
     return (
-      <div className="flex flex-col bg-light rounded-lg space-y-1 p-4">
-        <p>{`${label}`}</p>
-        <p className={`text-accent text-sm ${!isFirstEntry && "pb-2"}`}>
+      <div
+        className={`flex flex-col ${
+          isMobile ? "bg-light/90" : "bg-light"
+        } rounded-lg space-y-1 p-4`}
+      >
+        <p className={isMobile ? "text-sm" : ""}>{`${
+          graphType === "exponential"
+            ? dayjs()
+                .add(label as number, "day")
+                .format("YYYY-MM-DD")
+            : label
+        }`}</p>
+        <p
+          className={`text-accent ${isMobile ? "text-xs" : "text-sm"} ${
+            !isFirstEntry && "pb-2"
+          }`}
+        >
           {!swapped ? "Arcane" : "Sacred"} Power : {payload[0].value}
         </p>
         {!isFirstEntry && <hr className="tooltip-divider" />}
@@ -279,7 +309,11 @@ const Graph = ({ symbols, swapped }: Props) => {
               const occurrences = symbolEntries.length - 1;
 
               // Check if the entry is the second one (for upgrade message)
-              const isSecondEntry = label === dayjs().format("YYYY-MM-DD");
+              const isSecondEntry =
+                label ===
+                (graphType === "exponential"
+                  ? 0
+                  : dayjs().format("YYYY-MM-DD"));
 
               // If the symbol is ready to upgrade
               const upgradeReady =
@@ -288,11 +322,15 @@ const Graph = ({ symbols, swapped }: Props) => {
               return (
                 <div key={symbol.name} className="flex flex-col text-tertiary">
                   <div className="flex items-center space-x-1">
-                    <p className="text-sm">{`${symbol.name} : ${
-                      symbolEntries[0].entryLevel - 1
-                    }`}</p>
-                    <HiArrowSmRight fill="#8c8c8c" className="opacity-75" />
-                    <p className="text-sm">{`${
+                    <p className={isMobile ? "text-xs" : "text-sm"}>{`${
+                      symbol.name
+                    } : ${symbolEntries[0].entryLevel - 1}`}</p>
+                    <HiArrowSmRight
+                      size={isMobile ? 14 : 16}
+                      fill="#8c8c8c"
+                      className="opacity-75"
+                    />
+                    <p className={isMobile ? "text-xs" : "text-sm"}>{`${
                       symbolEntries[0].entryLevel + occurrences
                     }`}</p>
                   </div>
@@ -329,15 +367,38 @@ const Graph = ({ symbols, swapped }: Props) => {
 
     ticks.push(maxPower);
 
-    ////console.log(ticks.length);
-
     if (ticks[0] !== ticks[2]) {
-      ////console.log(currentPower, maxPower);
       // ! Bandaid bug fix | ticks[1] sacred would be stuck in middle of Y axis (arcane)
-      ////console.log(ticks);
       setYAxisTicks(ticks);
     } else {
       setYAxisTicks([]);
+    }
+  }, [graphSymbols]);
+
+  // Generate 7 evenly spaced ticks for the X axis
+  useLayoutEffect(() => {
+    if (
+      !isValid(currentPower) ||
+      !isValid(maxPower) ||
+      currentPower === 0 ||
+      maxPower === 0
+    ) {
+      setXAxisTicks([]);
+    }
+
+    const ticks = [0];
+
+    for (let i = 1; i < 7; i++) {
+      ticks.push(Math.ceil((i * maxDays) / 7));
+    }
+
+    ticks.push(maxDays);
+
+    if (ticks[0] !== ticks[2] && graphType === "exponential") {
+      // ! Bandaid bug fix | ticks[1] sacred would be stuck in middle of Y axis (arcane)
+      setXAxisTicks(ticks);
+    } else {
+      setXAxisTicks([]);
     }
   }, [graphSymbols]);
 
@@ -373,11 +434,19 @@ const Graph = ({ symbols, swapped }: Props) => {
     }
 
     // Find the first graphSymbol entry that matches or is closest to the provided power
-    const tempDateToPower = graphSymbols.find(
+    let tempDateToPower = graphSymbols.find(
       (entry) => entry.power >= Math.ceil(targetPower / 10) * 10
     )?.date;
 
-    setDateToPower(tempDateToPower as string);
+    if (graphType === "exponential") {
+      tempDateToPower = dayjs()
+        .add(tempDateToPower as number, "day")
+        .format("YYYY-MM-DD");
+    }
+
+    if (dayjs(tempDateToPower).isValid()) {
+      setDateToPower(tempDateToPower as string);
+    }
   }, [currentPower, targetPower, graphSymbols]);
 
   // Get the date or error message for the attainment date of the target power
@@ -392,17 +461,22 @@ const Graph = ({ symbols, swapped }: Props) => {
       ${isMobile ? " be over" : " be greater than"} ${currentPower}`;
   };
 
-  // Reset targetPower if symbols are swapped 
+  // Reset targetPower if symbols are swapped
   useEffect(() => {
     setTargetPower(NaN);
-  }, [swapped])
+  }, [swapped]);
 
   // Reset targetPower if symbols are disabled
   useEffect(() => {
     if (currentPower === 0) {
       setTargetPower(NaN);
     }
-  }, [currentPower])
+  }, [currentPower]);
+
+  // Format the X axis (change from days to date)
+  const formatXAxis = (tick: number) => {
+    return dayjs().add(tick, "day").format("YYYY-MM-DD");
+  };
 
   /* ―――――――――――――――――――― Render Logic ――――――――――――――――――― */
 
@@ -415,60 +489,74 @@ const Graph = ({ symbols, swapped }: Props) => {
               isMobile && "space-y-2"
             }`} // ! Bug. Spacing weird if this is put in responsively with Tailwind.
           >
-            <div className="flex flex-col justify-center items-center bg-dark rounded-lg mb-2 py-4 px-6 laptop:px-8">
-              <p>{!swapped ? "Arcane" : "Sacred"} Power</p>
-              <p className="text-accent laptop:text-lg pt-2.5">
-                {currentPower} / {enabledSymbols * (!swapped ? 220 : 110)}
-              </p>
-            </div>
+              <div className="flex flex-col justify-center items-center bg-dark rounded-lg mb-2 py-4 px-6 laptop:px-8">
+                <p>{!swapped ? "Arcane" : "Sacred"} Power</p>
+                <p className="text-accent laptop:text-lg pt-2.5">
+                  {currentPower} / {enabledSymbols * (!swapped ? 220 : 110)}
+                </p>
+              </div>
 
-            <div className="flex flex-col justify-center items-center bg-dark rounded-lg mb-2 py-4 px-6 laptop:px-8">
-              <div className="flex items-center space-x-3 tablet:space-x-3 pb-2.5">
-                <p>
-                  {isMobile
-                    ? `Target ${!swapped ? "Arcane" : "Sacred"} Power`
-                    : "When will"}
-                </p>
-                <Tooltip>
-                  <TooltipTrigger asChild={true}>
-                    <input
-                      type="number"
-                      className={`power-input h-[30px] w-[65px] ${
-                        graphSymbols.length === 1 &&
-                        "opacity-25 pointer-events-none select-none"
-                      }`}
-                      placeholder="Target"
-                      value={targetPower}
-                      onWheel={(e) => e.currentTarget.blur()}
-                      onChange={(e) => getTargetPowerDate(e.target.value)}
-                      tabIndex={graphSymbols.length === 1 ? -1 : 0}
-                    ></input>
-                  </TooltipTrigger>
-                  <TooltipContent className="tooltip">
-                    Calculate the date you'll <br></br>achieve the{" "}
-                    <span>specified power</span>
-                  </TooltipContent>
-                </Tooltip>
-                <p className={isMobile ? "hidden" : "block"}>
-                  {!swapped ? "arcane" : "sacred"} power be reached?
-                </p>
+              <div className={`flex flex-col justify-center items-center bg-dark rounded-lg tablet:mb-2 mb-1 py-4 px-6 laptop:px-8`}>
+                <div className="flex items-center space-x-3 tablet:space-x-3 pb-2.5">
+                  <p>
+                    {isMobile
+                      ? `Target ${!swapped ? "Arcane" : "Sacred"} Power`
+                      : "When will"}
+                  </p>
+                  <Tooltip>
+                    <TooltipTrigger asChild={true}>
+                      <input
+                        type="number"
+                        className={`power-input h-[30px] w-[65px] ${
+                          graphSymbols.length === 1 &&
+                          "opacity-25 pointer-events-none select-none"
+                        }`}
+                        placeholder="Target"
+                        value={targetPower}
+                        onWheel={(e) => e.currentTarget.blur()}
+                        onChange={(e) => getTargetPowerDate(e.target.value)}
+                        tabIndex={graphSymbols.length === 1 ? -1 : 0}
+                      ></input>
+                    </TooltipTrigger>
+                    <TooltipContent className="tooltip">
+                      Calculate the date you'll <br></br>achieve the{" "}
+                      <span>specified power</span>
+                    </TooltipContent>
+                  </Tooltip>
+                  <p className={isMobile ? "hidden" : "block"}>
+                    {!swapped ? "arcane" : "sacred"} power be reached?
+                  </p>
+                </div>
+                <div className="flex space-x-1.5">
+                  <p
+                    className={
+                      isMobile && targetPower > currentPower
+                        ? "block"
+                        : "hidden"
+                    }
+                  >
+                    Attainment Date:{" "}
+                  </p>
+                  <p className="text-accent laptop:text-lg">
+                    {getTargetPowerResponse()}
+                  </p>
+                </div>
               </div>
-              <div className="flex space-x-1.5">
-                <p
-                  className={
-                    isMobile && targetPower > currentPower ? "block" : "hidden"
-                  }
-                >
-                  Attainment Date:{" "}
-                </p>
-                <p className="text-accent laptop:text-lg">
-                  {getTargetPowerResponse()}
-                </p>
-              </div>
-            </div>
+            
           </div>
 
           <hr className="horizontal-divider" />
+
+          <div className="flex space-x-12 tablet:space-x-24 pb-6 tablet:pb-4">
+            <div className="flex items-center space-x-4" onClick={() => setGraphType("linear")}>
+              <div className={`${graphType === "linear" && "bg-accent"} border-[3px] border-secondary rounded-full h-[20px] w-[20px] transition-all`}></div>
+              <p>Linear</p>
+            </div>
+            <div className="flex items-center space-x-4" onClick={() => setGraphType("exponential")}>
+              <div className={`${graphType === "exponential" && "bg-accent"} border-[3px] border-secondary rounded-full h-[20px] w-[20px] transition-all`}></div>
+              <p>Exponential</p>
+            </div>
+          </div>
 
           <LineChart
             width={isMobile ? 290 : isTablet ? 600 : 950}
@@ -486,7 +574,8 @@ const Graph = ({ symbols, swapped }: Props) => {
               content={<CustomTooltip />}
             />
             <Line
-              type="monotone"
+              type="linear"
+              isAnimationActive={false}
               dataKey="power"
               name={`${!swapped ? "Arcane" : "Sacred"} Power`}
               stroke="#b18bd0"
@@ -504,9 +593,33 @@ const Graph = ({ symbols, swapped }: Props) => {
             />
             <XAxis
               dataKey="date"
+              type={graphType === "exponential" ? "number" : "category"}
               tickMargin={isMobile ? 5 : 10}
               minTickGap={isMobile ? 50 : 15}
+              domain={[
+                (min: number) => {
+                  if (isFinite(min)) {
+                    return 0;
+                  } else {
+                    return 0;
+                  }
+                },
+                (max: number) => {
+                  if (isFinite(max)) {
+                    return maxDays;
+                  } else {
+                    return 1;
+                  }
+                },
+              ]}
               stroke="#8c8c8c"
+              tickCount={graphType === "exponential" ? 10 : undefined}
+              ticks={graphType === "exponential" ? xAxisTicks : undefined}
+              tickFormatter={
+                graphType === "exponential"
+                  ? (tick) => formatXAxis(tick)
+                  : undefined
+              }
             />
             <YAxis
               dataKey="power"
